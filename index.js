ffc1060e34626563f34fca3921e3fc8abfef21bb
KarthyV <Kdon979@gmail.com>
2022-06-03 16:00:29 +0530

    port added

--- index.js
+++ index.js
@@ -67,5 +67,5 @@ app.delete("/movies/:id", async (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on PORT 5000");
+  console.log(`Server is running on PORT ${process.env.PORT}`);
 });
