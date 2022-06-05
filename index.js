fe64f9a1980aaeee1c4da43ea1b1354a7d66a629
KarthyV <Kdon979@gmail.com>
2022-06-05 14:54:18 +0530

    cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
